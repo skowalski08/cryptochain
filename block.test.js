80d76f1b6bdcec1642f20d3391f816fb245b78dc
Sara Kowalski <sarakowalski08@gmail.com>
2021-01-07 20:02:47 -0500

    feat: test for mine block

--- block.test.js
+++ block.test.js
@@ -26,4 +26,26 @@ describe('Block', () => {
       expect(genesisBlock).toEqual(GENESIS_DATA)
     })
   })
+
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const mineBlock = Block.mineBlock({ lastBlock, data });
+
+    it('returns a Block instance', () => {
+      expect(mineBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the last `lastHash` to be the `hash` of the lastBlock', () => {
+      expect(mineBlock.lastHash).toEqual(lastBlock.hash);
+    })
+
+    it('sets the `data`', () => {
+      expect(mineBlock.data).toEqual(data);
+    })
+
+    it('sets a `timestamp`', () => {
+      expect(mineBlock.timestamp).not.toEqual(undefined)
+    })
+  })
 })
