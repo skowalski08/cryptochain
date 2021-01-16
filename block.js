aee21a3d1851282090226f4f0cca94d1ae95af33
Sara Kowalski <sarakowalski08@gmail.com>
2021-01-15 22:49:27 -0500

    feat: refactored code so difficulty will adjust based on time

--- block.js
+++ block.js
@@ -16,14 +16,15 @@ const cryptoHash = require('./crypto-hash');
    }
 
    static mineBlock({ lastBlock, data }) {
-    let hash, timestamp;
     const lastHash = lastBlock.hash;
-    const { difficulty } = lastBlock;
+    let hash, timestamp;
+    let { difficulty } = lastBlock;
     let nonce = 0;
 
     do {
       nonce++;
       timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp })
       hash = cryptoHash(timestamp, lastHash, data, difficulty, nonce)
     } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
@@ -39,9 +40,11 @@ const cryptoHash = require('./crypto-hash');
 
    static adjustDifficulty({ originalBlock, timestamp }) {
      const { difficulty } = originalBlock;
-     const difference = timestamp - originalBlock.timestamp;
+    //  const difference = timestamp - originalBlock.timestamp;
 
-     if (difference > MINE_RATE) {
+     if (difficulty < 1) return 1;
+
+     if ((timestamp - originalBlock.timestamp) > MINE_RATE) {
        return difficulty - 1;
      }
 
