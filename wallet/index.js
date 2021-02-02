6fb9ef9c362711433e0746fc263825eab35f4ce9
Sara Kowalski <sarakowalski08@gmail.com>
2021-02-01 21:11:38 -0500

    feat: added sign feature to wallet and verification of signature helper func

--- wallet/index.js
+++ wallet/index.js
@@ -1,12 +1,17 @@
 const { STARTING_BALANCE } = require('../config')
+const cryptoHash = require('../util/crypto-hash')
 const { ec } = require('../util')
+
 
 class Wallet{
   constructor() {
     this.balance = STARTING_BALANCE;
+    this.keyPair = ec.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
+  }
 
-    const keyPair = ec.genKeyPair();
-    this.publicKey = keyPair.getPublic().encode('hex');
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data))
   }
 };
 
