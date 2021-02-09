244c2b551545c84879686121806c27bbe406875f
Sara Kowalski <sarakowalski08@gmail.com>
2021-02-09 17:44:22 -0500

    fix: refacor cryptohash func

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -3,7 +3,7 @@ const crypto = require('crypto')
 const cryptoHash = (...inputs) => {
   const hash = crypto.createHash('sha256')
 
-  hash.update(inputs.sort().join(' '));
+  hash.update(inputs.map(input => JSON.stringify(input)).sort().join(' '));
 
   //digest is a term in cryptography to represent the result of a hash, digest is in hex form
   return hash.digest('hex')
