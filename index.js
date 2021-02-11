3d81b22e5be154b8be18d070ae11c8ed611a4f9c
Sara Kowalski <sarakowalski08@gmail.com>
2021-02-11 16:38:25 -0500

    feat: sync transact pool map on connect

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool, wallet });
 
 // setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -48,18 +48,29 @@ app.post('/api/transact', (req, res) => {
   }
 
   transactionPool.setTransaction(transaction)
-  console.log('transactionPool', transactionPool);
-
+  pubsub.broadcastTransaction(transaction);
   res.json({ type: 'success', transaction });
 })
 
-const syncChains = () => {
+app.get('/api/transaction-pool-map', (req, res) => {
+  res.json(transactionPool.transactionMap)
+})
+
+const syncWithRootState = () => {
   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const rootChain = JSON.parse(body);
 
       console.log('replace chain on a sync with', rootChain)
       blockchain.replaceChain(rootChain);
+    }
+  })
+
+  request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const rootTransactionPoolMap = JSON.parse(body);
+      console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+      transactionPool.setMap(rootTransactionPoolMap);
     }
   })
 }
@@ -75,6 +86,6 @@ app.listen(PORT, () => {
   console.log(`WE BE LISTENIN localhost:${PORT}`)
 
   if (PORT !== DEFAULT_PORT) {
-    syncChains();
+    syncWithRootState();
   }
 });
