5f13f00e467c5c84c68fa184cf56791921b70066
Sara Kowalski <sarakowalski08@gmail.com>
2021-02-26 14:13:37 -0500

    feat: added test for clearing transactions in blockchain

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,6 +1,7 @@
 const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
 const Wallet = require('./index')
+const Blockchain = require('../blockchain')
 
 describe('TransactionPool', () => {
   let transactionPool, transaction, senderWallet;
@@ -76,6 +77,30 @@ describe('TransactionPool', () => {
       transactionPool.clear();
 
       expect(transactionPool.transactionMap).toEqual({})
+    })
+  })
+
+  describe('clearBlockchainTransactions()', () => {
+    it('clears the pool of any existing blockchain transactions', () => {
+      const blockchain = new Blockchain();
+      const expectedTransactionMap = {}
+
+      for (i = 0; i < 6; i++){
+        const transaction = new Wallet().createTransaction({
+          recipient: 'foo', amount: 20
+        })
+
+        transactionPool.setTransaction(transaction)
+
+        if (i % 2 === 0) {
+          blockchain.addBlock({ data: [transaction] })
+        } else {
+          expectedTransactionMap[transaction.id] = transaction
+        }
+      }
+
+      transactionPool.clearBlockchainTransactions({ chain: blockchain.chain })
+      expect(transactionPool.transactionMap).toEqual(expectedTransactionMap)
     })
   })
 });
