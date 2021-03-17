718eac0cb1628decd4ed76a5383d90e4b170eb3c
Sara Kowalski <sarakowalski08@gmail.com>
2021-03-17 13:40:16 -0400

    feat: updated child block to displayTransaction data

--- client/src/components/ChildBlock.js
+++ client/src/components/ChildBlock.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 
 class ChildBlock extends Component {
-  render() {
-    const { timestamp, hash, data } = this.props.block;
-    const hashDisplay = `${hash.substring(0, 15)}...`;
+  state = { displayTransaction: false };
+
+  toggleTransaction = () => {
+    this.setState({ displayTransaction: !this.state.displayTransaction })
+  };
+
+  get displayTransaction() {
+    const { data } = this.props.block
     const stringifiedData = JSON.stringify(data);
     const dataDisplay = stringifiedData.length > 35 ?
       `${stringifiedData.substring(0, 35)}...` :
       stringifiedData;
 
+    return <div>Data: {dataDisplay}</div>
+  }
+
+  render() {
+    const { timestamp, hash } = this.props.block;
+    const hashDisplay = `${hash.substring(0, 15)}...`;
+
+
     return (
       <div className='ChildBlock'>
         <div>Hash: {hashDisplay}</div>
         <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
-        <div>Data: {dataDisplay}</div>
+        {this.displayTransaction}
       </div>
     )
   }
